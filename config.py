bc7be309f05e9a011a78e0dec4abbf7ec551600a
Boyan Zhou <steven.b.zhou@foxmail.com>
2021-11-09 15:59:34 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+data_url = "https://develop.exnaton.com/api/v2/meterdata/measurement"
+auth_url = "https://develop.exnaton.com/api/v2/auth/auth"
+rest_api_url = "http://127.0.0.1:5000/"
+email = ""
+password = ""
+credential = {"email" : email, "password" : password}
+db_username = ""
+db_password = ""
+db_host = "127.0.0.1"
+db_port = 5432
